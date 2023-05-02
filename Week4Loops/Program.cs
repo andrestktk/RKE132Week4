ff6fc0e7fb917fe5e10890a6ca99914f4a940775
Andrestktk <andres.altmets@tktk.ee>
2023-05-02 22:23:13 +0300

    add eek4loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//for-loop
+
+int i = 0;
+
+for(i = 0; i < 5; i++)  // i++ = i + 1
+{
+    Console.WriteLine("We do need no education!");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+
+
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine($"cpu ha genereated {cpuRandom}");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int userNumbre = Int32.Parse(Console.ReadLine());
+
+    if (userNumbre == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        //loopActive = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Oops, try again!");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
